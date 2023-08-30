e4c26219574dddff965a944a9bbdf55a0e4cf2d5
Sam Mead <samjamead@gmail.com>
2023-08-30 23:16:45 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './_components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
