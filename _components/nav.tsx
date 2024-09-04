2b6a1ea565203d76af2a673a0e604132ae51ec22
Sam Mead <samjamead@gmail.com>
2024-09-04 20:52:36 +0100

    Update the nav for pages

--- _components/nav.tsx
+++ _components/nav.tsx
@@ -1,63 +1,42 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-
 const menuItems = [
   {
-    name: 'All',
-    link: '/',
+    name: 'Leaderboard',
+    link: '/leaderboard',
   },
   {
-    name: 'Writeups',
-    link: '/?category=Writeups',
-  },
-  {
-    name: 'Quotes',
-    link: '/?category=Quotes',
+    name: 'Honours Board',
+    link: '/honours',
   },
 ]
 
 export default function Nav() {
-  const searchParams = useSearchParams()
+  const path = usePathname()
 
-  const category = searchParams.get('category')
+  console.log(path)
 
   return (
     <nav>
       <ul className='flex items-center justify-between gap-4'>
-        {/* {menuItems.map(({ name, link }) => {
+        {menuItems.map(({ name, link }) => {
           return (
             <li key={name}>
               <Link
                 href={link}
-                className={`text-sm px-2 py-1 rounded ${
-                  category && link.includes(category)
-                    ? "text-white font-normal bg-purple-500/30"
-                    : "text-foreground/80 hover:text-white hover:bg-purple-500/20"
-                }  transition-all`}
+                className={`rounded px-2 py-1 text-sm ${
+                  path && link === path
+                    ? 'bg-purple-500/30 font-normal text-white'
+                    : 'text-foreground/80 hover:bg-purple-500/20 hover:text-white'
+                } transition-all`}
               >
                 {name}
               </Link>
             </li>
-          );
-        })} */}
-        <li>
-          <Link
-            href='/leaderboard'
-            className={`rounded px-2 py-1 text-sm font-normal text-foreground/80 transition-all hover:bg-purple-500/20 hover:text-white`}
-          >
-            Leaderboard
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/honours'
-            className={`rounded px-2 py-1 text-sm font-normal text-foreground/80 transition-all hover:bg-purple-500/20 hover:text-white`}
-          >
-            Honours Board
-          </Link>
-        </li>
+          )
+        })}
       </ul>
     </nav>
   )
