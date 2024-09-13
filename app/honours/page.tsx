7c63c7cdcab6028df386cc8929c7122e42b23553
Sam Mead <samjamead@gmail.com>
2024-09-13 11:57:02 +0100

    Update honours board

--- app/honours/page.tsx
+++ app/honours/page.tsx
@@ -1,4 +1,5 @@
 import honours from '../../_data/honours.json'
+import spiritHonours from '../../_data/spiritHonours.json'
 
 export default async function Index() {
   return (
@@ -26,6 +27,34 @@ export default async function Index() {
                   <td className='p-2 font-medium'>{entry.winner}</td>
                   <td className='hidden p-2 md:table-cell'>
                     {entry.second || ''}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className='lg:mb-12'>
+          <h3 className='mb-2'>The Spirit of The Cup Trophy</h3>
+          <table className='mb-8 w-full table-auto text-xs md:text-sm'>
+            <thead>
+              <tr className='border-b text-left'>
+                <th className='p-2'>Year</th>
+                <th className='p-2'>Venue</th>
+                <th className='p-2'>Winner</th>
+                <th className='hidden p-2 md:table-cell'>Points</th>
+
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {spiritHonours.map((entry) => (
+                <tr key={entry.year} className='even:bg-foreground/5'>
+                  <td className='p-2'>{entry.year}</td>
+                  <td className='p-2'>{entry.venue || ''}</td>
+                  <td className='p-2 font-medium'>{entry.winner}</td>
+                  <td className='hidden p-2 md:table-cell'>
+                    {entry.points || ''}
                   </td>
                 </tr>
               ))}
